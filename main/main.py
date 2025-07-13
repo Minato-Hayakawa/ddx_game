db35a2aa129b4933732bcb9afcd5ee95cf41b693
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2025-07-13 15:07:20 +0900

    StandクラスのHorizonalMoveメソッドを修正。VerticalMoveメソッドを削除

--- main/main.py
+++ main/main.py
@@ -70,9 +70,6 @@ class Stand:
                  xSpeed:int,
                  xVector:bool,
                  yVector:bool,
-                 BlockCount:np.array,
-                 BlockXPosition:np.array,
-                 BlockYPosition:np.array,
                  StandxPosition:int,
                  StandyPosition:int):
         self.xSpeed=xSpeed
@@ -81,119 +78,17 @@ class Stand:
 
         self.StandxPosition=StandxPosition
         self.StandyPosition=StandyPosition
+        self.BotanHandlerObj=BotanHandler()
     
     def HorizonalMove(self):
-        if self.xVector==True and self.xSpeed<SCREEN_WIDTH-12:
-            self.ddx_xPosition+=self.xSpeed
-            self.xVector=True
-        elif self.xVector==True and self.xSpeed>=SCREEN_WIDTH-12:
-            self.ddx_xPosition+=self.xSpeed
-            self.xVector=False
-        elif self.xVector==False and self.xSpeed>0:
-            self.ddx_xPosition-=self.xSpeed
-            self.xVector=False
-        elif self.xVector==False and self.xSpeed<=0:
-            self.ddx_xPosition+=self.xSpeed
-            self.xVector=True
-    
-    def VerticalMove(self):
-        if self.yVector==True and self.ySpeed<SCREEN_HIGHT-16-19:
-            self.ddx_yPosition+=self.ySpeed
-            self.yVector=True
-        elif self.yVector==True and self.ySpeed>=SCREEN_HIGHT-16-19:
-            self.ddx_yPosition-=self.ySpeed
-            self.yVector=False
-        for i in range(7):
-            for j in range(5):
-                if self.yVector==False and self.ddx_yPosition==self.BlockYPosition[j] and self.BlockXPosition[i]<=self.ddx_xPosition<=self.BlockXPosition[i+1]:
-                    self.BlockCount[i][j]-=1
-                    self.yVector=True
-        # elif self.yVector==False and self.ySpeed>self.top[self.topnum]:
-        #     self.ySpeed+=-1
-        #     self.yVector=False
-        # elif self.yVector==False and self.ySpeed<=self.top[self.topnum]:
-        #     self.speedup=False
-        #     self.xSpeed+=1
-        #     self.yVector=True
-            
-        #     for i in range(7):
-        #         for j in range(5)
-        #         if i*16<=self.xSpeed<=i*16+16 and self.count1[i]==0 and j==5:
-        #             self.flags1[i]=True
-        #             self.count1[i]=1
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==1 and j==5:
-        #             self.flags2[i]=True
-        #             self.count1[i]=2
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==2 and j==5:
-        #             self.flags3[i]=True
-        #             self.count1[i]=3
-        #         elif self.count1==[3]*7 and j==5:
-        #             self.Block[i][j]=False
-        #             self.count1=[0]*7
-        #             self.flags1=[False]*7
-        #             self.flags2=[False]*7
-        #             self.flags3=[False]*7
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==0 and j==4:
-        #             self.flags1[i]=True
-        #             self.count1[i]=1
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==1 and j==4:
-        #             self.flags2[i]=True
-        #             self.count1[i]=2
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==2 and j==4:
-        #             self.flags3[i]=True
-        #             self.count1[i]=3
-        #         elif self.count1==[3]*7 and j==4:
-        #             self.topnum+=-1
-        #             self.count1=[0]*7
-        #             self.flags1=[False]*7
-        #             self.flags2=[False]*7
-        #             self.flags3=[False]*7
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==0 and j==3:
-        #             self.flags1[i]=True
-        #             self.count1[i]=1
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==1 and j==3:
-        #             self.flags2[i]=True
-        #             self.count1[i]=2
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==2 and j==3:
-        #             self.flags3[i]=True
-        #             self.count1[i]=3
-        #         elif self.count1==[3]*7 and j==3:
-        #             self.topnum+=-1
-        #             self.count1=[0]*7
-        #             self.flags1=[False]*7
-        #             self.flags2=[False]*7
-        #             self.flags3=[False]*7
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==0 and j==2:
-        #             self.flags1[i]=True
-        #             self.count1[i]=1
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==1 and j==2:
-        #             self.flags2[i]=True
-        #             self.count1[i]=2
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==2 and j==2:
-        #             self.flags3[i]=True
-        #             self.count1[i]=3
-        #         elif self.count1==[3]*7 and j==2:
-        #             self.topnum+=-1
-        #             self.count1=[0]*7
-        #             self.flags1=[False]*7
-        #             self.flags2=[False]*7
-        #             self.flags3=[False]*7
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==0 and j==1:
-        #             self.flags1[i]=True
-        #             self.count1[i]=1
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==1 and self.topnum==1:
-        #             self.flags2[i]=True
-        #             self.count1[i]=2
-        #         elif i*16<=self.xSpeed<=i*16+16 and self.count1[i]==2 and self.topnum==1:
-        #             self.flags3[i]=True
-        #             self.count1[i]=3
-        #         elif self.count1==[3]*7 and self.topnum==1:
-        #             self.topnum+=-1
-        #             self.count1=[0]*7
-        #             self.flags1=[False]*7
-        #             self.flags2=[False]*7
-        #             self.flags3=[False]*7
-    
+        if self.BotanHandlerObj.Right() and self.StandxPosition<SCREEN_WIDTH-32: #押され続けているのを検知
+            self.StandxPosition+=self.xSpeed
+            if self.BotanHandlerObj.Up():
+                self.StandxPosition+=self.xSpeed*2
+        elif self.BotanHandlerObj.Left() and self.xPosition>0:
+            self.StandxPosition-=self.xSpeed
+            if self.BotanHandlerObj.Up():
+                self.StandxPosition-=self.xSpeed*2
     def StandReflection(self):
             if self.yVector==True and self.ddx_yPosition==SCREEN_HIGHT*3//4-8:
                 if self.ddx_yPosition-16<=self.ddx_xPosition<=self.ddx_yPosition+36:
@@ -245,20 +140,11 @@ class App:
     def update(self): #フレーム更新時の処理
         if pyxel.btnp(pyxel.KEY_ESCAPE): #押した瞬間を検知
             pyxel.quit()
+    
         
-        if self.BotanHandlerObj.Right() and self.StandObj.xPosition<SCREEN_WIDTH-32: #押され続けているのを検知
-            self.StandObj.StandxPosition+=self.StandObj.xSpeed
-            if self.BotanHandlerObj.Up():
-                self.StandObj.StandxPosition+=self.StandObj.xSpeed*2
-        elif self.BotanHandlerObj.Left() and self.StandObj.xPosition>0:
-            self.StandObj.StandxPosition-=self.StandObj.xSpeed
-            if self.BotanHandlerObj.Up():
-                self.StandObj.StandxPosition-=self.StandObj.xSpeed*2
-        #台の操作
-        
-        self.StandObj.HorizonalMove()
-        self.StandObj.VerticalMove()
-        self.StandObj.Reflection()
+        self.ddxObj.HorizonalMove()
+        self.ddxObj.VerticalMove()
+        self.StandObj.StandReflection()
                    
     def draw(self):
         pyxel.cls(pyxel.COLOR_NAVY)
